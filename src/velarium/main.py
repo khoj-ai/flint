4a6ad3c47206e7d1709ce3904dc59f3a4ea6791b
Debanjum Singh Solanky <debanjum@gmail.com>
2023-08-02 22:46:38 -0700

    Start server on running the main app entrypoint as a script

--- src/velarium/main.py
+++ src/velarium/main.py
@@ -21,14 +21,18 @@ app = FastAPI()
 def chat():
     return {"message": "Hello World"}
 
-# Main Function
 def start_server(app, host="127.0.0.1", port=8488, socket=None):
-    logger.info("🌖 velarium is ready to use")
+    logger.info("🌖 Velarium is ready to use")
     if socket:
         uvicorn.run(app, proxy_headers=True, uds=socket, log_level="debug", use_colors=True, log_config=None)
     else:
         uvicorn.run(app, host=host, port=port, log_level="debug", use_colors=True, log_config=None)
-    logger.info("🌒 Stopping velarium")
+    logger.info("🌒 Stopping Velarium")
+
 
 def run():
     start_server(app)
+
+
+if __name__ == "__main__":
+    run()
