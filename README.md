6043dfd0fc936c24c43b9119d934b5410173e565
sabaimran <narmiabas@gmail.com>
2023-08-02 20:23:19 -0700

    Create basic project skeleton with ingle FastAPI server endpoint
    - Create pyproject.toml, LICENSE, Readme

---
+++ README.md
@@ -0,0 +1,35 @@
+<p align="center"><img src="src/velarium/interface/web/assets/icons/khoj-logo-sideways.svg" width="230" alt="Khoj Logo"></p>
+
+<div align="center">
+
+[![test](https://github.com/khoj-ai/velarium/actions/workflows/test.yml/badge.svg)](https://github.com/khoj-ai/velarium/actions/workflows/test.yml)
+[![pypi](https://github.com/khoj-ai/velarium/actions/workflows/pypi.yml/badge.svg)](https://pypi.org/project/khoj-assistant/)
+
+</div>
+
+<div align="center">
+<b>An AI personal assistant for your digital brain</b>
+</div>
+
+<br />
+
+<div align="center">
+
+[📜 Read Docs](https://docs.khoj.dev)
+<span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+[🌍 Visit Website](https://khoj.dev)
+<span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+[💬 Get Involved](https://discord.gg/BDgyabRM6e)
+
+</div>
+
+<div align="center">
+
+***
+
+velarium is an open source, AI personal assistant.<br />
+You can converse with it anytime over Whatsapp, SMS or your Web Browser.<br />
+
+***
+
+</div>
---
+++ src/velarium/main.py
@@ -0,0 +1,34 @@
+# Standard Packages
+import logging
+
+# External Packages
+from fastapi import FastAPI
+from rich.logging import RichHandler
+import uvicorn
+
+# Setup Logger
+rich_handler = RichHandler(rich_tracebacks=True)
+rich_handler.setFormatter(fmt=logging.Formatter(fmt="%(message)s", datefmt="[%X]"))
+logging.basicConfig(handlers=[rich_handler])
+
+logger = logging.getLogger("velarium")
+
+# Initialize the Application Server
+app = FastAPI()
+
+# Routes
+@app.get("/chat")
+def chat():
+    return {"message": "Hello World"}
+
+# Main Function
+def start_server(app, host="127.0.0.1", port=8488, socket=None):
+    logger.info("🌖 velarium is ready to use")
+    if socket:
+        uvicorn.run(app, proxy_headers=True, uds=socket, log_level="debug", use_colors=True, log_config=None)
+    else:
+        uvicorn.run(app, host=host, port=port, log_level="debug", use_colors=True, log_config=None)
+    logger.info("🌒 Stopping velarium")
+
+def run():
+    start_server(app)
