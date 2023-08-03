42d69ecdcd33860d5bc618207554f988f57080b1
Debanjum Singh Solanky <debanjum@gmail.com>
2023-08-02 22:43:30 -0700

    Expose simple conversation agent with memory via the /chat API
    Use Langchain to instantiate a Conversation Agent with Memory
    Conversation agent uses OpenAI as the chat model

--- src/velarium/main.py
+++ src/velarium/main.py
@@ -6,6 +6,16 @@ from fastapi import FastAPI
 from rich.logging import RichHandler
 import uvicorn
 
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
 # Setup Logger
 rich_handler = RichHandler(rich_tracebacks=True)
 rich_handler.setFormatter(fmt=logging.Formatter(fmt="%(message)s", datefmt="[%X]"))
@@ -16,10 +26,30 @@ logger = logging.getLogger("velarium")
 # Initialize the Application Server
 app = FastAPI()
 
+# Initialize the Conversational Chain with Memory
+llm = ChatOpenAI(temperature=0)
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(
+            f"""
+You are Khoj, a friendly, smart and helpful personal assistant.
+Use your general knowledge and our past conversations to provide assistance.
+""".strip()
+        ),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ]
+)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+converse = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
+
+
 # Routes
 @app.get("/chat")
-def chat():
-    return {"message": "Hello World"}
+def chat(q: str):
+    result = converse({"question": q})
+    return {"message": result["text"]}
+
 
 def start_server(app, host="127.0.0.1", port=8488, socket=None):
     logger.info("🌖 Velarium is ready to use")
