8c83e84256aee5a698c492cf5e704ca845dee4b8
sabaimran <narmiabas@gmail.com>
2024-01-23 12:23:07 +0530

    Update welcome message to talk about images, give info about khoj cloud

--- src/flint/constants.py
+++ src/flint/constants.py
@@ -5,18 +5,19 @@ KHOJ_WHATSAPP_PROD = "whatsapp:+18488004242"
 KHOJ_WHATSAPP_DEBUG = "whatsapp:+14155238886"
 KHOJ_INTRO_MESSAGE = f"""
 Nice to meet you! I am Khoj, your dedicated personal AI 👋🏽. I can help you with:
-- 📜 Free-form journaling and note-taking
 - 🧠 Answering general knowledge questions
 - 💡 Ideating over new ideas
 - 🖊️ Being a scratchpad for your thoughts, links, concepts that you want to remember
+- 🌄 Making images from your own words (try typing `/dream`)
+- 📝 Retrieving information from your own knowledge base shared with Khoj on the cloud. See https://khoj.dev for more info
 
 You can also send me voice messages in your native language 🎙️.
 
-I'm constantly learning and improving. I'm still in beta, so please report any bugs or issues to my creators on GitHub: https://github.com/khoj-ai/flint/issues
+I'm constantly learning and improving, so please report any issues to my creators on GitHub: https://github.com/khoj-ai/flint/issues
 """.strip()
 
 KHOJ_FAILED_AUDIO_TRANSCRIPTION_MESSAGE = f"""
-Sorry, I wasn't able to understand your voice message this time. Could you please try typing out your message or send a shorter audio file? If you'd like to help me improve, email my creators at team@khoj.dev.
+Sorry, I wasn't able to understand your voice message this time. Could you please try typing out your message or send a shorter message? If you'd like to help me improve, email my creators at team@khoj.dev.
 """.strip()
 
 KHOJ_API_URL = os.getenv("KHOJ_API_URL", "https://khoj.dev")
