d470eb680a3c53d4e6e2008f17b4db5c3172f49c
Debanjum Singh Solanky <debanjum@gmail.com>
2024-01-25 22:27:43 +0530

    Split dev API endpoint into separate router. Organize main API router
    - Group all the API endpoint functions at the top of the api router file
    - Move /dev/chat into a separate router and turn it on/off based on
      debug during app configuration

--- src/flint/configure.py
+++ src/flint/configure.py
@@ -1,10 +1,12 @@
 # Standard Packages
+import os
 import logging
 
 # External Packages
 from fastapi import FastAPI
 
 
+DEBUG = os.getenv("DEBUG", False)
 logger = logging.getLogger(__name__)
 
 
@@ -12,6 +14,8 @@ def configure_routes(app: FastAPI):
     "Configure the API Routes"
     logger.info("Including routes")
     from flint.routers.api import api
-
     app.include_router(api, prefix="/api")
 
+    if DEBUG:
+        from flint.routers.dev import dev
+        app.include_router(dev, prefix="/dev")
